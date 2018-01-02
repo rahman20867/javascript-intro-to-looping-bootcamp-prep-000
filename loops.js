9ca73d04f7f631bfc22ee3e5ef39a2b8dd371af0
Marvin Andrade <marvin@lionguardsecurity.com>
2018-01-02 13:36:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
-function forLoop(){
+function forLoop(array){
   for(let i = 0 ; i < 25; i++){
-    console.log( "I am ${i} strange loop" );
+    console.log( array.push("I am ${i} strange loop") );
   }
 }
